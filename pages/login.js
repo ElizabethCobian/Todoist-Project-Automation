2908fe7847915c3d6bee01d918e67281a00f91e7
“ElizabethCobian” <elizabeth.cobian7@gmail.com>
2021-01-26 17:27:51 -0600

    login udpate file

--- pages/login.js
+++ pages/login.js
@@ -1,26 +1,15 @@
 var login = function(){
 
-    var email_input = element(by.id('email'));
-    var password_input = element(by.id('password'));
-    var loginButton = element(by.css('.submit_btn'));
-
     this.get = function(url){
+        
         browser.get(url);
     };
 
-    this.enterEmail = function(emailAdded){
-        email_input.sendKeys(emailAdded);
+    this.existingUser = function(){
+        var email_input = element(by.id('email'));
+        var password_input = element(by.id('password'));
+        var loginButton = element(by.css('.submit_btn'));
     };
-
-    this.enterPass = function(passAdded){
-        password_input.sendKeys(passAdded);
-    };
-
-    this.clickLogin = function(){
-        loginButton.click();
-    };
-
-    this.
 
 };
 module.exports = new login();
